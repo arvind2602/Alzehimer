beb4dac6014be3a118a6df67c3484dca1a13dc16
arvind2602 <guptaarvind2602@gmail.com>
2023-04-20 20:27:49 +0530

    Modified settings

--- alzeimer/alzeimer/settings.py
+++ alzeimer/alzeimer/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f!fatfa3764z14)u&52=b$k35#+@%otlh0g4(&&ja!5!5m+cwx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
